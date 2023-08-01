5de5d8a27098cec9ca957ea5de398fc3168ee51f
Sebastian Herrmann <sebastian@herrherrmann.net>
2023-08-01 16:52:14 +0200

    Add basic theme with dark mode, remove some example content

--- index.js
+++ index.js
@@ -1,21 +1,4 @@
-// Zoom constants. Define Max, Min, increment and default values
-const ZOOM_INCREMENT = 0.2
-const MAX_ZOOM = 5
-const MIN_ZOOM = 0.3
-const DEFAULT_ZOOM = 1
-
-function firstUnpinnedTab(tabs) {
-	for (let tab of tabs) {
-		if (!tab.pinned) {
-			return tab.index
-		}
-	}
-}
-
-/**
- * listTabs to switch to
- */
-function listTabs() {
+function initialize() {
 	getCurrentWindowTabs().then((tabs) => {
 		let tabsList = document.getElementById('tabs-list')
 		let currentTabs = document.createDocumentFragment()
@@ -41,10 +24,10 @@ function listTabs() {
 	})
 }
 
-document.addEventListener('DOMContentLoaded', listTabs)
+document.addEventListener('DOMContentLoaded', initialize)
 
 function getCurrentWindowTabs() {
-	return browser.tabs.query({currentWindow: true})
+	return browser.tabs.query({ currentWindow: true })
 }
 
 document.addEventListener('click', (e) => {
@@ -58,123 +41,26 @@ document.addEventListener('click', (e) => {
 		})
 	}
 
-	if (e.target.id === 'tabs-move-beginning') {
-		callOnActiveTab((tab, tabs) => {
-			let index = 0
-			if (!tab.pinned) {
-				index = firstUnpinnedTab(tabs)
-			}
-			console.log(`moving ${tab.id} to ${index}`)
-			browser.tabs.move([tab.id], {index})
+	if (e.target.id === 'tabs-create') {
+		browser.tabs.create({
+			url: 'https://developer.mozilla.org/en-US/Add-ons/WebExtensions',
 		})
-	}
-
-	if (e.target.id === 'tabs-move-end') {
-		callOnActiveTab((tab, tabs) => {
-			let index = -1
-			if (tab.pinned) {
-				let lastPinnedTab = Math.max(0, firstUnpinnedTab(tabs) - 1)
-				index = lastPinnedTab
-			}
-			browser.tabs.move([tab.id], {index})
-		})
-	}
-
-	else if (e.target.id === 'tabs-duplicate') {
-		callOnActiveTab((tab) => {
-			browser.tabs.duplicate(tab.id)
-		})
-	}
-
-	else if (e.target.id === 'tabs-reload') {
-		callOnActiveTab((tab) => {
-			browser.tabs.reload(tab.id)
-		})
-	}
-
-	else if (e.target.id === 'tabs-remove') {
-		callOnActiveTab((tab) => {
-			browser.tabs.remove(tab.id)
-		})
-	}
-
-	else if (e.target.id === 'tabs-create') {
-		browser.tabs.create({url: 'https://developer.mozilla.org/en-US/Add-ons/WebExtensions'})
-	}
-
-	else if (e.target.id === 'tabs-create-reader') {
-		browser.tabs.create({url: 'https://developer.mozilla.org/en-US/Add-ons/WebExtensions', openInReaderMode: true})
-	}
-
-	else if (e.target.id === 'tabs-alertinfo') {
+	} else if (e.target.id === 'tabs-alertinfo') {
 		callOnActiveTab((tab) => {
 			let props = ''
 			for (let item in tab) {
-				props += `${ item } = ${ tab[item] } \n`
+				props += `${item} = ${tab[item]} \n`
 			}
 			alert(props)
 		})
-	}
-
-	else if (e.target.id === 'tabs-add-zoom') {
-		callOnActiveTab((tab) => {
-			let gettingZoom = browser.tabs.getZoom(tab.id)
-			gettingZoom.then((zoomFactor) => {
-				//the maximum zoomFactor is 5, it can't go higher
-				if (zoomFactor >= MAX_ZOOM) {
-					alert('Tab zoom factor is already at max!')
-				} else {
-					let newZoomFactor = zoomFactor + ZOOM_INCREMENT
-					//if the newZoomFactor is set to higher than the max accepted
-					//it won't change, and will never alert that it's at maximum
-					newZoomFactor = newZoomFactor > MAX_ZOOM ? MAX_ZOOM : newZoomFactor
-					browser.tabs.setZoom(tab.id, newZoomFactor)
-				}
-			})
-		})
-	}
-
-	else if (e.target.id === 'tabs-decrease-zoom') {
-		callOnActiveTab((tab) => {
-			let gettingZoom = browser.tabs.getZoom(tab.id)
-			gettingZoom.then((zoomFactor) => {
-				//the minimum zoomFactor is 0.3, it can't go lower
-				if (zoomFactor <= MIN_ZOOM) {
-					alert('Tab zoom factor is already at minimum!')
-				} else {
-					let newZoomFactor = zoomFactor - ZOOM_INCREMENT
-					//if the newZoomFactor is set to lower than the min accepted
-					//it won't change, and will never alert that it's at minimum
-					newZoomFactor = newZoomFactor < MIN_ZOOM ? MIN_ZOOM : newZoomFactor
-					browser.tabs.setZoom(tab.id, newZoomFactor)
-				}
-			})
-		})
-	}
-
-	else if (e.target.id === 'tabs-default-zoom') {
-		callOnActiveTab((tab) => {
-			let gettingZoom = browser.tabs.getZoom(tab.id)
-			gettingZoom.then((zoomFactor) => {
-				if (zoomFactor == DEFAULT_ZOOM) {
-					alert('Tab zoom is already at the default zoom factor')
-				} else {
-					browser.tabs.setZoom(tab.id, DEFAULT_ZOOM)
-				}
-			})
-		})
-	}
-
-	else if (e.target.classList.contains('switch-tabs')) {
+	} else if (e.target.classList.contains('switch-tabs')) {
 		let tabId = +e.target.getAttribute('href')
 
-		browser.tabs.query({
-			currentWindow: true
-		}).then((tabs) => {
+		browser.tabs.query({ currentWindow: true }).then((tabs) => {
 			for (let tab of tabs) {
 				if (tab.id === tabId) {
 					browser.tabs.update(tabId, {
-						active: true
+						active: true,
 					})
 				}
 			}
@@ -182,22 +68,4 @@ document.addEventListener('click', (e) => {
 	}
 
 	e.preventDefault()
-})
-
-//onRemoved listener. fired when tab is removed
-browser.tabs.onRemoved.addListener((tabId, removeInfo) => {
-	console.log(`The tab with id: ${tabId}, is closing`)
-
-	if(removeInfo.isWindowClosing) {
-		console.log('Its window is also closing.')
-	} else {
-		console.log('Its window is not closing')
-	}
-})
-
-//onMoved listener. fired when tab is moved into the same window
-browser.tabs.onMoved.addListener((tabId, moveInfo) => {
-	let startIndex = moveInfo.fromIndex
-	let endIndex = moveInfo.toIndex
-	console.log(`Tab with id: ${tabId} moved from index: ${startIndex} to index: ${endIndex}`)
 })
