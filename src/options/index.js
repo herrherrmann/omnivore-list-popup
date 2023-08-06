f9ef99a57f9a134a4ba52c3fb1a37d6cbbe8027d
Sebastian Herrmann <sebastian@herrherrmann.net>
2023-08-06 12:58:32 +0200

    Improve styles on settings page

--- src/options/index.js
+++ src/options/index.js
@@ -11,6 +11,9 @@ async function saveOptions(event) {
 	event.preventDefault()
 	const apiKey = document.querySelector(apiKeyFieldSelector).value
 	await saveApiKey(apiKey)
+	const messageElement = document.querySelector('#message')
+	messageElement.classList.add('success')
+	messageElement.textContent = 'Saved!'
 }
 
 document.addEventListener('DOMContentLoaded', restoreOptions)
