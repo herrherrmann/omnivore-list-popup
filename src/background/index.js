d48326fa8bd6ad522af1423d2552b26b43610099
Sebastian Herrmann <sebastian@herrherrmann.net>
2024-04-30 19:06:54 +0200

    Show status on icon badge when adding a new page via shortcut

--- src/background/index.js
+++ src/background/index.js
@@ -5,6 +5,34 @@ import { getActiveTab } from '../services/tabs'
 browser.commands.onCommand.addListener(async (command) => {
 	if (command === 'add_current_page') {
 		const activeTab = await getActiveTab()
-		await addLink(activeTab.url)
+		await browser.browserAction.setBadgeBackgroundColor({
+			color: '#2ac3a2',
+			tabId: activeTab.id,
+		})
+		await browser.browserAction.setBadgeText({
+			text: '+',
+			tabId: activeTab.id,
+		})
+		try {
+			await addLink(activeTab.url)
+		} catch (error) {
+			await browser.browserAction.setBadgeBackgroundColor({
+				color: '#c50042',
+				tabId: activeTab.id,
+			})
+			await browser.browserAction.setBadgeText({
+				text: 'X',
+				tabId: activeTab.id,
+			})
+		} finally {
+			await browser.browserAction.setBadgeText({
+				text: '',
+				tabId: activeTab.id,
+			})
+			await browser.browserAction.setBadgeBackgroundColor({
+				color: null,
+				tabId: activeTab.id,
+			})
+		}
 	}
 })
