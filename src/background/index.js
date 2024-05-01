a68e04c12be3bd91452d6e9c9e929d50c2682024
Sebastian Herrmann <sebastian@herrherrmann.net>
2024-05-01 11:51:13 +0200

    Show status on icon badge when adding a new page via shortcut (#39)

--- src/background/index.js
+++ src/background/index.js
@@ -1,10 +1,25 @@
 import browser from 'webextension-polyfill'
 import { addLink } from '../services/api'
+import {
+	resetBadgeText,
+	setErrorBadgeText,
+	setSuccessBadgeText,
+} from '../services/badge'
 import { getActiveTab } from '../services/tabs'
 
 browser.commands.onCommand.addListener(async (command) => {
 	if (command === 'add_current_page') {
 		const activeTab = await getActiveTab()
-		await addLink(activeTab.url)
+		setSuccessBadgeText('+', activeTab.id)
+		try {
+			await addLink(activeTab.url)
+			await setSuccessBadgeText('✔︎', activeTab.id)
+		} catch (error) {
+			await setErrorBadgeText('✗', activeTab.id)
+		} finally {
+			setTimeout(() => {
+				resetBadgeText(activeTab.id)
+			}, 2_000)
+		}
 	}
 })
