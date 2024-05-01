fa41535e97e173ec30e6863e323378bda0e51f6c
Sebastian Herrmann <sebastian@herrherrmann.net>
2024-05-01 12:26:16 +0200

    Fix browserAction access in Chrome (Manifest v3)

--- src/services/badge.js
+++ src/services/badge.js
@@ -1,20 +1,27 @@
+import browser from 'webextension-polyfill'
+
+const action =
+	browser.runtime.getManifest().manifest_version === 3
+		? browser.action
+		: browser.browserAction
+
 export async function setSuccessBadgeText(text, tabId) {
-	await browser.browserAction.setBadgeBackgroundColor({
+	await action.setBadgeBackgroundColor({
 		color: '#2ac3a2',
 		tabId,
 	})
-	await browser.browserAction.setBadgeText({ text, tabId })
+	await action.setBadgeText({ text, tabId })
 }
 
 export async function setErrorBadgeText(text, tabId) {
-	await browser.browserAction.setBadgeBackgroundColor({
+	await action.setBadgeBackgroundColor({
 		color: '#c50042',
 		tabId,
 	})
-	await browser.browserAction.setBadgeText({ text, tabId })
+	await action.setBadgeText({ text, tabId })
 }
 
 export async function resetBadgeText(tabId) {
-	await browser.browserAction.setBadgeText({ text: '', tabId })
-	await browser.browserAction.setBadgeBackgroundColor({ color: null, tabId })
+	await action.setBadgeText({ text: '', tabId })
+	await action.setBadgeBackgroundColor({ color: null, tabId })
 }
