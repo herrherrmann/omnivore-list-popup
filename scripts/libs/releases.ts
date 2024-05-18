f4fa1c346ea6ed61a509070563e1652edaacb56c
Sebastian Herrmann <sebastian@herrherrmann.net>
2024-05-18 11:22:06 +0200

    Fix release script for Chrome

--- scripts/libs/releases.ts
+++ scripts/libs/releases.ts
@@ -5,21 +5,19 @@ import { zipRelease, zipSources } from './zip-files.ts'
 export async function createReleases() {
 	/* eslint-disable no-console */
 	const vite = await import('vite')
-	const targetBrowsers: TargetBrowser[] = ['firefox', 'chrome']
-	for (const targetBrowser of targetBrowsers) {
-		console.log(`⏳ Creating release for ${targetBrowser} ...`)
-		try {
-			await vite.build(viteConfig)
-			await zipRelease(targetBrowser)
-			if (targetBrowser === 'firefox') {
-				await zipSources(targetBrowser)
-			}
-		} catch (error) {
-			console.error(
-				`❌ Error while creating release for ${targetBrowser}: `,
-				error,
-			)
+	const targetBrowser = process.env.TARGET as TargetBrowser
+	console.log(`⏳ Creating release for ${targetBrowser} ...`)
+	try {
+		await vite.build(viteConfig)
+		await zipRelease(targetBrowser)
+		if (targetBrowser === 'firefox') {
+			await zipSources(targetBrowser)
 		}
-		console.log(`✅ Release for ${targetBrowser} created!`)
+	} catch (error) {
+		console.error(
+			`❌ Error while creating release for ${targetBrowser}: `,
+			error,
+		)
 	}
+	console.log(`✅ Release for ${targetBrowser} created!`)
 }
