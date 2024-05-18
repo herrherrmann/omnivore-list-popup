778bf049f4332feb42c3182a8822f2fbd0d1eef2
Sebastian Herrmann <sebastian@herrherrmann.net>
2024-05-18 11:24:44 +0200

    Simplify release script files

--- scripts/create-releases.ts
+++ scripts/create-releases.ts
@@ -1,3 +1,24 @@
-import { createReleases } from './libs/releases.ts'
+import viteConfig from '../vite.config.ts'
+import type { TargetBrowser } from './libs/types.d.ts'
+import { zipRelease, zipSources } from './libs/zip-files.ts'
 
-createReleases()
+export async function createReleases() {
+	/* eslint-disable no-console */
+	const vite = await import('vite')
+	const targetBrowser = process.env.TARGET as TargetBrowser
+	console.log(`⏳ Creating release for ${targetBrowser} ...`)
+	try {
+		await vite.build(viteConfig)
+		await zipRelease(targetBrowser)
+		if (targetBrowser === 'firefox') {
+			await zipSources(targetBrowser)
+		}
+	} catch (error) {
+		console.error(
+			`❌ Error while creating release for ${targetBrowser}: `,
+			error,
+		)
+	}
+	console.log(`✅ Release for ${targetBrowser} created!`)
+	/* eslint-enable no-console */
+}
